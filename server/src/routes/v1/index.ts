5489963f2b62d9c0a80818460d24452e64f502c3
SolareFlame <zeldac332@gmail.com>
2026-01-21 22:18:27 +0100

    add character routes to main router

--- server/src/routes/v1/index.ts
+++ server/src/routes/v1/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import license_routes from './license_routes.js';
+import character_routes from "./character_routes.js";
 
 const router = Router();
 
 router.use('/licenses', license_routes);
+router.use('/characters', character_routes);
 
 export default router;
