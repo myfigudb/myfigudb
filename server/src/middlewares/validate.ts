7f94f9d2147f3e86ea5df95d9d62bdfcbbf82175
SolareFlame <zeldac332@gmail.com>
2026-01-21 22:19:22 +0100

    Validate with body, params and query params

--- server/src/middlewares/validate.ts
+++ server/src/middlewares/validate.ts
@@ -1,21 +1,41 @@
-import { Request, Response, NextFunction } from 'express';
-import { ZodObject } from "zod";
+import {Request, Response, NextFunction} from 'express';
+import {ZodError, ZodObject} from "zod";
 
-export const validate = (schema: ZodObject) => {
+interface Validation {
+    body?: ZodObject;
+    params?: ZodObject;
+    query?: ZodObject;
+}
+
+export const validate = (schemas: Validation) => {
     return async (req: Request, res: Response, next: NextFunction) => {
-        const result = await schema.safeParseAsync(req.body);
+        try {
+            if (schemas.params) {
+                req.params = await schemas.params.parseAsync(req.params) as any;
+            }
 
-        if (!result.success) {
-            return res.status(400).json({
-                message: "Validation failed",
-                errors: result.error.issues.map(i => ({
-                    path: i.path.join('.'),
-                    message: i.message
-                }))
-            });
+            if (schemas.query) {
+                req.query = await schemas.query.parseAsync(req.query) as any;
+            }
+
+            if (schemas.body) {
+                req.body = await schemas.body.parseAsync(req.body);
+            }
+
+            return next();
+
+        } catch (error) {
+            if (error instanceof ZodError) {
+                return res.status(400).json({
+                    message: "Validation failed",
+                    errors: error.issues.map(issue => ({
+                        path: issue.path.join('.'),
+                        message: issue.message
+                    }))
+                });
+            }
+
+            return res.status(500).json({ message: "Internal server error during validation" });
         }
-
-        req.body = result.data;
-        return next();
     };
 };
