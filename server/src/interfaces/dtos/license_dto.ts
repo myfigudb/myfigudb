ae954cb606cd06bcd299847e5689fdea2346ca7f
SolareFlame <zeldac332@gmail.com>
2026-01-21 21:26:13 +0100

    fix: correct schema name in license DTO and routes

--- server/src/interfaces/dtos/license_dto.ts
+++ server/src/interfaces/dtos/license_dto.ts
@@ -1,7 +1,7 @@
 import {z} from "zod";
 
-export const licenseShema = z.object({
+export const licenseSchema = z.object({
     name: z.string().min(1).trim(),
 })
 
-export type LicenseDTO = z.infer<typeof licenseShema>;
+export type LicenseDTO = z.infer<typeof licenseSchema>;
