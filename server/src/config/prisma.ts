6220319e3deb3b63a681873d6cc02a6e8f64416d
SolareFlame <zeldac332@gmail.com>
2026-01-21 21:12:18 +0100

    update Prisma client configuration to use PostgreSQL (with ESM)

--- server/src/config/prisma.ts
+++ server/src/config/prisma.ts
@@ -1,3 +1,17 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../generated/prisma/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+import pg from 'pg';
 
-export const prisma = new PrismaClient()
+import 'dotenv/config';
+
+const { Pool } = pg;
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({
+    adapter,
+});
