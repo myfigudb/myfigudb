30d9afda700dbc7c1109e6908d48793552a08199
SolareFlame <zeldac332@gmail.com>
2026-01-21 18:08:06 +0100

    add LicenseService for managing licenses

---
+++ server/src/services/LicenseService.ts
@@ -0,0 +1,70 @@
+import { prisma } from "../config/prisma";
+import { License, Prisma } from "@prisma/client";
+
+export class LicenseService {
+
+    /**
+     * Retrieve a license by its unique ID.
+     * @returns The license or null if not found.
+     */
+    async getLicenseById(id: string): Promise<License | null> {
+        return prisma.license.findUnique({
+            where: { id }
+        });
+    }
+
+    /**
+     * Retrieve a license by its unique Name.
+     * @returns The license or null if not found.
+     */
+    async getLicenseByName(name: string): Promise<License | null> {
+        return prisma.license.findUnique({
+            where: { name }
+        });
+    }
+
+    async createLicense(data: Prisma.LicenseCreateInput): Promise<License> {
+        return prisma.license.create({
+            data: data
+        });
+    }
+
+    /**
+     * Update an existing license.
+     * @throws {Prisma.PrismaClientKnownRequestError} If the ID does not exist.
+     */
+    async updateLicense(id: string, data: Prisma.LicenseUpdateInput): Promise<License> {
+        return prisma.license.update({
+            where: { id },
+            data: data
+        });
+    }
+
+    /**
+     * Delete a license.
+     * @throws {Prisma.PrismaClientKnownRequestError} If the ID does not exist.
+     */
+    async deleteLicense(id: string): Promise<License> {
+        return prisma.license.delete({
+            where: { id }
+        });
+    }
+
+    /**
+     * Find licenses with similar names using trigram similarity (PostgreSQL pg_trgm).
+     *
+     * IMPORTANT : We use strict SQL here, so we must use the database table name ("license"),
+     * defined in {@link ../../prisma/catalog.prisma catalog.prisma} via @@map("license")
+     *
+     * @param name          Name to search
+     * @param threshold     Trigger threshold (default: 0.3)
+     * @param limit         Number of results to return (default: 1)
+     */
+    async getLicenseBySimilarityName(name: string, threshold: number = 0.3, limit: number = 1): Promise<License[]> {
+        return prisma.$queryRaw<License[]>`
+            SELECT *
+            FROM "license"
+            WHERE similarity(name, ${name}) > ${threshold} LIMIT ${limit};
+        `;
+    }
+}
