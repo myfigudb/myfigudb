1587b07a6d7fe6c2aaf1e78bcfbe351ec640da12
SolareFlame <zeldac332@gmail.com>
2026-01-22 11:47:45 +0100

    New routes added : auth and users

--- server/src/routes/v1/index.ts
+++ server/src/routes/v1/index.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express';
+
 import license_routes from './license_routes.js';
 import character_routes from "./character_routes.js";
+import auth_routes from "./auth_routes.js";
+import user_routes from "./user_routes.js";
 
 const router = Router();
 
+
+
+
 router.use('/licenses', license_routes);
 router.use('/characters', character_routes);
+
+router.use('/auth', auth_routes);
+router.use('/users', user_routes);
 
 export default router;
