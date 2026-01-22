41ebd349110d7bba0d337dde8b9ec7e68c7161a0
SolareFlame <zeldac332@gmail.com>
2026-01-22 17:13:21 +0100

    Export params type

--- server/src/interfaces/dtos/params/common.ts
+++ server/src/interfaces/dtos/params/common.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 export const paramsIdSchema = z.object({
     id: z.uuid({ message: "Format UUID invalide" })
 });
+export type ParamsId = z.infer<typeof paramsIdSchema>;
+
 
 export const paramsNameSchema = z.object({
     name: z.string({ message: "Format String invalide" }).min(1).trim(),
