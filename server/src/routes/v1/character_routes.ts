ab13df2ecef56b3ccbfe7f947c04f2e0518f7258
SolareFlame <zeldac332@gmail.com>
2026-01-22 11:49:06 +0100

    New auth and role checking as a test

--- server/src/routes/v1/character_routes.ts
+++ server/src/routes/v1/character_routes.ts
@@ -3,15 +3,16 @@ import { validate } from '../../middlewares/validate.js';
 
 import {characterSchema} from "../../interfaces/dtos/character_dto.js";
 import {CharacterController} from "../../controllers/characterController.js";
+
 import {paramsIdSchema} from "../../interfaces/dtos/common.js";
+import {verifyToken} from "../../middlewares/auth.js";
+import {verifyRole} from "../../middlewares/role.js";
 
 const router = Router();
 const controller = new CharacterController();
 
-
-router.post('/', validate({body: characterSchema }), (req, res) => controller.create(req, res));
-router.get('/', (req, res) => controller.findAll(req, res));
+router.post('/', verifyToken, validate({body: characterSchema }), (req, res) => controller.create(req, res));
+router.get('/', verifyToken, verifyRole("default"), (req, res) => controller.findAll(req, res));
 router.get('/:id', validate({ params: paramsIdSchema }), controller.findById);
-
 
 export default router;
