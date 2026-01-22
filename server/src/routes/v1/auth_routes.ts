11a3293f69312cad5e2c7b6bb99080f30200b9a2
SolareFlame <zeldac332@gmail.com>
2026-01-22 11:49:53 +0100

    New auth route to connect

---
+++ server/src/routes/v1/auth_routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { validate } from '../../middlewares/validate.js';
+
+import {AuthController} from "../../controllers/authController.js";
+import {authSchema} from "../../interfaces/dtos/auth_dto.js";
+
+
+const router = Router();
+const controller = new AuthController();
+
+
+router.post('/', validate({body: authSchema }), (req, res) => controller.login(req, res));
+
+
+
+export default router;
