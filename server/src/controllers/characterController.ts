590ecf315b524723cc0cd72d478fb1403da18eb6
SolareFlame <zeldac332@gmail.com>
2026-01-22 17:13:09 +0100

    Medias gestion

--- server/src/controllers/characterController.ts
+++ server/src/controllers/characterController.ts
@@ -1,7 +1,13 @@
-import { Request, Response } from 'express';
-import {CharacterService} from "../services/characterService.js";
+import {Request, RequestHandler, Response} from 'express';
+import {CharacterService} from "../services/database/characterService.js";
+import {StorageService} from "../services/storageService.js";
+import {MediaService} from "../services/database/mediaService.js";
+import {ParamsId} from "../interfaces/dtos/params/common.js";
 
 const service = new CharacterService();
+
+const storage_service = new StorageService();
+const media_service = new MediaService();
 
 export class CharacterController {
 
@@ -39,6 +45,41 @@ export class CharacterController {
             return res.status(200).json(character);
         } catch (error) {
             console.error("Error getting all character with error:", error);
+            return res.status(500).json({ message: "Internal server error" });
+        }
+    }
+
+    uploadMedias: RequestHandler<ParamsId> = async (req, res, next) => {
+        try {
+            // 1. Character ID
+            const { id } = req.params;
+
+            if(!(await service.existsCharacter(id))) {
+                return res.status(404).json({ message: "Character not found" });
+            }
+
+            // 2. Files
+            const files = req.files as Express.Multer.File[];
+
+            if (!files || files.length === 0) {
+                return res.status(400).json({ message: "No files uploaded" });
+            }
+
+            const media_hashes: string[] = [];
+
+            for (const file of files) {
+                const uploaded = await storage_service.uploadFile(file.buffer, file.mimetype, 'characters');
+
+                await media_service.ensureMediaExists(uploaded.hash, file.mimetype);
+                media_hashes.push(uploaded.hash);
+            }
+
+            const updated_character = await service.attachMedias(id, media_hashes);
+
+            return res.status(200).json(updated_character);
+
+        } catch (error) {
+            console.error("Error uploading character medias:", error);
             return res.status(500).json({ message: "Internal server error" });
         }
     }
