c5d5bf0f40ff5483fb1405a24fa483d158383cf6
SolareFlame <zeldac332@gmail.com>
2026-01-22 11:50:19 +0100

    Auth dto to check connect with slug or email

---
+++ server/src/interfaces/dtos/auth_dto.ts
@@ -0,0 +1,14 @@
+import {z} from "zod";
+
+export const authSchema = z.object({
+    password: z.string().min(1, "Password is required"),
+
+    email: z.email().optional(),
+    slug: z.string().min(3).optional(),
+})
+
+    .refine((data) => data.email || data.slug, {
+        message: "You must provide either an email or a slug",
+        path: ["email"],
+    });
+
