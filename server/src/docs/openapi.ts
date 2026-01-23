a96b0cebf1d1e9fb0866d97d4fa0383c6f7ff14e
ChairGamertag87 <moieee1977@gmail.com>
2026-01-24 00:19:24 +0100

    Add user docs to OpenAPI generator

--- server/src/docs/openapi.ts
+++ server/src/docs/openapi.ts
@@ -2,11 +2,13 @@ import { OpenApiGeneratorV3 } from '@asteasolutions/zod-to-openapi';
 
 import { characterRegistry } from './domains/character_docs.js';
 import {licenseRegistry} from "./domains/license_docs.js";
+import {userRegistry} from "./domains/user_docs.js";
 
 export const generateOpenApiDocs = () => {
     const generator = new OpenApiGeneratorV3([
         ...characterRegistry.definitions,
         ...licenseRegistry.definitions,
+        ...userRegistry.definitions,
     ]);
 
     return generator.generateDocument({
