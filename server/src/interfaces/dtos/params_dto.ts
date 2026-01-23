6641caa5c447d4bd170848d667d6d3bd0bef52d2
ChairGamertag87 <moieee1977@gmail.com>
2026-01-24 00:52:16 +0100

    Add paramsSlugSchema to params DTOs

--- server/src/interfaces/dtos/params_dto.ts
+++ server/src/interfaces/dtos/params_dto.ts
@@ -11,7 +11,13 @@ export type ParamsIdDTO = z.infer<typeof paramsIdSchema>;
 
 export const paramsNameSchema = z.object({
     name: z.string({ message: "Format String invalide" }).min(1).trim(),
+
 })
+
+export const paramsSlugSchema = z.object({
+    name: z.string({ message: "Format String invalide" }).min(1).trim(),
+})
+
 export type ParamsNameDTO = z.infer<typeof paramsNameSchema>;
 
 
