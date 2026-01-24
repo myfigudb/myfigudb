6dba1b1a690e343003809be3cc460efa96316e2c
ChairGamertag87 <moieee1977@gmail.com>
2026-01-24 20:12:03 +0100

    Add material docs to OpenAPI generator

--- server/src/docs/openapi.ts
+++ server/src/docs/openapi.ts
@@ -4,6 +4,7 @@ import { characterRegistry } from './domains/character_docs.js';
 import {licenseRegistry} from "./domains/license_docs.js";
 import {userRegistry} from "./domains/user_docs.js";
 import {figureRegistry} from "./domains/figure_docs.js";
+import {materialRegistry} from "./domains/material_doc.js";
 
 export const generateOpenApiDocs = () => {
     const generator = new OpenApiGeneratorV3([
@@ -11,6 +12,7 @@ export const generateOpenApiDocs = () => {
         ...licenseRegistry.definitions,
         ...userRegistry.definitions,
         ...figureRegistry.definitions,
+        ...materialRegistry.definitions,
     ]);
 
     return generator.generateDocument({
---
+++ server/src/interfaces/dtos/entities/material_dto.ts
@@ -0,0 +1,42 @@
+import {z} from "zod";
+
+import {extendZodWithOpenApi} from "@asteasolutions/zod-to-openapi";
+import {Material, Media} from "../../../generated/prisma/client.js";
+
+import {CharacterInput} from "./character_dto.js";
+extendZodWithOpenApi(z)
+
+/**
+ * INPUT DTO: create a new Material
+ */
+export const createMaterialSchema = z.object({
+    name: z.string().min(1).trim(),
+})
+
+export type CreateMaterialDTO = z.infer<typeof createMaterialSchema>;
+
+
+/**
+ * OUTPUT DTO: Material response
+ * NOTE: DO NOT EXECUTE, TYPE DEFINITION FOR OPENAPI
+ * @see toMaterialDTO
+ */
+export const materialResponseSchema = z.object({
+    id: z.uuid(),
+    name: z.string(),
+})
+
+export type MaterialResponse = z.infer<typeof materialResponseSchema>;
+
+
+/**
+ * OUTPUT toDTO: Material response
+ */
+export type MaterialInput = Material;
+
+export const toMaterialDTO = (source: MaterialInput): MaterialResponse => {
+    return {
+        id: source.id,
+        name: source.name
+    };
+};
