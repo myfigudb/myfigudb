4dea9c678f5d4d8d8c9dce04c7f7fc495a040976
ChairGamertag87 <moieee1977@gmail.com>
2026-01-24 11:37:25 +0100

    Refactor Character model and add EditorService

---
+++ server/src/services/database/editorService.ts
@@ -0,0 +1,58 @@
+import {pclient} from "../../config/prisma.js";
+import {Editor, Prisma} from "../../generated/prisma/client.js";
+
+export class EditorService {
+
+    /**
+     * Retrieve an Editor by its unique ID.
+     */
+    async getEditorById(id: string): Promise<Editor | null> {
+        return pclient.editor.findUnique({
+            where: { id }
+        });
+    }
+
+    /**
+     * Retrieve an Editor by its unique ID.
+     */
+    async getEditorByName(name: string): Promise<Editor | null> {
+        return pclient.editor.findFirst({
+            where: { name }
+        });
+    }
+
+    /**
+     * Retrieve all Editors.
+     */
+    async getAllEditors(): Promise<Editor[]> {
+        return pclient.editor.findMany();
+    }
+
+    /**
+     * Create a new Editor.
+     */
+    async createEditor(data: Prisma.EditorCreateInput): Promise<Editor> {
+        return pclient.editor.create({
+            data: data
+        });
+    }
+
+    /**
+     * Update an existing Editor.
+     */
+    async updateEditor(id: string, data: Prisma.EditorUpdateInput): Promise<Editor> {
+        return pclient.editor.update({
+            where: { id },
+            data: data
+        });
+    }
+
+    /**
+     * Delete an Editor.
+     */
+    async deleteEditor(id: string): Promise<Editor> {
+        return pclient.editor.delete({
+            where: { id }
+        });
+    }
+}
