938028e120544621fd388f13819c267ff12aaba6
ChairGamertag87 <moieee1977@gmail.com>
2026-01-24 23:30:52 +0100

    Add editor docs to OpenAPI generator

---
+++ server/src/interfaces/dtos/entities/editor_dto.ts
@@ -0,0 +1,42 @@
+import {z} from "zod";
+
+import {extendZodWithOpenApi} from "@asteasolutions/zod-to-openapi";
+import {Editor, Media} from "../../../generated/prisma/client.js";
+
+import {CharacterInput} from "./character_dto.js";
+extendZodWithOpenApi(z)
+
+/**
+ * INPUT DTO: create a new Editor
+ */
+export const createEditorSchema = z.object({
+    name: z.string().min(1).trim(),
+})
+
+export type CreateEditorDTO = z.infer<typeof createEditorSchema>;
+
+
+/**
+ * OUTPUT DTO: Editor response
+ * NOTE: DO NOT EXECUTE, TYPE DEFINITION FOR OPENAPI
+ * @see toEditorDTO
+ */
+export const editorResponseSchema = z.object({
+    id: z.uuid(),
+    name: z.string(),
+})
+
+export type EditorResponse = z.infer<typeof editorResponseSchema>;
+
+
+/**
+ * OUTPUT toDTO: Editor response
+ */
+export type EditorInput = Editor;
+
+export const toEditorDTO = (source: EditorInput): EditorResponse => {
+    return {
+        id: source.id,
+        name: source.name
+    };
+};
