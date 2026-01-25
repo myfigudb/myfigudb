29391873f5ca2cdc5fcd18e84d7f01d79ed9ff5d
SolareFlame <zeldac332@gmail.com>
2026-01-25 18:22:37 +0100

    Add CORS policy

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import routes from "./routes/index.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // http://localhost:3000/api/
 app.use('/api', routes);
