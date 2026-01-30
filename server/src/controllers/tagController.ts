7ecce0ed645bf74b7195ef213e236ab0d4089d55
SolareFlame <zeldac332@gmail.com>
2026-01-30 22:17:58 +0100

    Tag Survey setup

--- server/src/controllers/tagController.ts
+++ server/src/controllers/tagController.ts
@@ -2,36 +2,42 @@ import {RequestHandler} from 'express';
 import {TagService} from "../services/database/figure/tagService.js";
 import jwt from 'jsonwebtoken';
 
+import {VoteDTO} from "../interfaces/dtos/vote_dto.js";
 
-import {ParamsIdDTO} from "../interfaces/dtos/params_dto.js";
-import {} from "../interfaces/dtos/entities/figure_dto.js";
+interface SurveyPayload {
+    survey_id: string;
+    user_id: string;
+}
 
 const service = new TagService();
 
 export class TagController {
-
-    //TODO remplacer l'id param par l'id du JWT d'auth
-    survey: RequestHandler<ParamsIdDTO> = async (req, res) => {
+    survey: RequestHandler = async (req, res) => {
         try {
-            const {id} = req.params;
-            const survey = await service.getTagValidationSurvey(id);
+            if (!req.user) {
+                return res.status(401).json({message: "Unauthorized"});
+            }
+
+            const survey = await service.getTagValidationSurvey(req.user.id);
 
             if (!survey) {
                 return res.status(404).json({message: "No survey available"});
             }
 
+            const payload: SurveyPayload = {
+                survey_id: survey.id,
+                user_id: req.user.id
+            };
+
+            const secret_key = process.env.JWT_TRIAL_SECRET || 'default_secret_key'
             const survey_token = jwt.sign(
-                {
-                    tid: survey.id,
-                    uid: id
-                },
-                process.env.JWT_SURVEY_SECRET as string,
+                payload,
+                secret_key,
                 {expiresIn: '15m'}
             );
 
             return res.status(200).json({
                 token: survey_token
-                //TODO infos
             });
 
         } catch (error) {
@@ -40,21 +46,22 @@ export class TagController {
         }
     }
 
-    //TODO remplacer l'id param par l'id du JWT d'auth
-    vote: RequestHandler<ParamsIdDTO> = async (req, res) => {
-        const {id} = req.params;
-        const {token, is_positive} = req.body;
+    vote: RequestHandler<{}, any, VoteDTO> = async (req, res) => {
+        if (!req.user) {
+            return res.status(401).json({message: "Unauthorized"});
+        }
 
+        const {token, vote} = req.body;
 
         try {
-            //TODO faire un service de verification JWT étant donné qu'on utilise plusieurs fois la func ?
-            const payload = jwt.verify(token, process.env.JWT_TRIAL_SECRET as string) as { fid: string, uid: string };
+            const secret_key = process.env.JWT_TRIAL_SECRET || 'default_secret_key'
+            const payload = jwt.verify(token, secret_key) as SurveyPayload;
 
-            if (payload.uid !== id) {
+            if (payload.user_id !== req.user.id) {
                 return res.status(403).json({message: "This trial was not assigned to you"});
             }
 
-            //vote
+            await service.registerTagValidationVote(payload.user_id, payload.survey_id, vote) //survey = rdm figure_tag
 
             return res.status(200).json({message: "Vote registered successfully"});
 
