ccf4fc308b346c4da3596f2dd2ccfdb7c76d6f3a
SolareFlame <zeldac332@gmail.com>
2026-01-30 22:17:23 +0100

    routes tags added

--- server/src/routes/v1/index.ts
+++ server/src/routes/v1/index.ts
@@ -4,8 +4,9 @@ import license_routes from './license_routes.js';
 import character_routes from "./character_routes.js";
 import auth_routes from "./auth_routes.js";
 import user_routes from "./user_routes.js";
-import figureRouter from './figure_routes.js';
-import editorRouter from './editor_routes.js';
+import figure_routes from './figure_routes.js';
+import editor_routes from './editor_routes.js';
+import tag_routes from "./tag_routes.js";
 
 const router = Router();
 
@@ -18,8 +19,10 @@ router.use('/characters', character_routes);
 router.use('/auth', auth_routes);
 router.use('/users', user_routes);
 
-router.use('/figures', figureRouter);
-router.use('/editors', editorRouter);
+router.use('/figures', figure_routes);
+router.use('/editors', editor_routes);
+
+router.use('/tags', tag_routes);
 
 
 export default router;
