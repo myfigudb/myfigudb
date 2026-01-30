dbabc561a887e6339c7f97311e25037ad640a7f8
SolareFlame <zeldac332@gmail.com>
2026-01-30 22:17:10 +0100

    Auth Request interface setup

--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -6,11 +6,7 @@ interface JWTPayload {
     role: string;
 }
 
-export interface AuthRequest extends Request {
-    user?: JWTPayload;
-}
-
-export const verifyToken = (req: AuthRequest, res: Response, next: NextFunction) => {
+export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
     const auth_header = req.headers['authorization'];
     const token = auth_header && auth_header.split(' ')[1];
 
@@ -22,7 +18,7 @@ export const verifyToken = (req: AuthRequest, res: Response, next: NextFunction)
     try {
         const secret_key = process.env.JWT_SECRET || 'default_secret_key';
 
-        req.user = jwt.verify(token, secret_key) as JWTPayload;
+        req.user = jwt.verify(token, secret_key) as Express.User;
 
         next();
     } catch (error) {
