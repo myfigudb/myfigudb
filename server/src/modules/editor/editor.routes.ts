bdb3cc96c44031bdb9faeb2261ec2c0cb8610364
SolareFlame <zeldac332@gmail.com>
2026-02-12 23:07:57 +0100

    Architecture update: Migrate 'Editor' Model to Monolith Architecture, refractor routes

--- server/src/modules/editor/editor.routes.ts
+++ server/src/modules/editor/editor.routes.ts
@@ -1,23 +1,23 @@
 import { Router } from 'express';
 import { validate } from '../../middlewares/validate.js';
-import { createEditorSchema } from "../../interfaces/dtos/entities/editor_dto.js";
-import { EditorController } from "../../controllers/editorController.js";
 import { verifyToken } from "../../middlewares/auth.js";
 import { verifyRole } from "../../middlewares/role.js";
+
+import { createEditorSchema } from "./editor.dto.js";
+import { EditorController } from "./editor.controller.js";
+
 
 const router = Router();
 const controller = new EditorController();
 
-// GET ALL
 router.get('/',
-    //verifyToken,
-    verifyRole("default"), // Optionnel selon ta config
+    verifyToken,
+    verifyRole("default"),
     controller.findAll
 );
 
-// CREATE
 router.post('/',
-    //verifyToken,
+    verifyToken,
     validate({ body: createEditorSchema }),
     controller.create
 );
