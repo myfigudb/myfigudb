689b306bdba35ccc069145b791839425773ed9d1
SolareFlame <zeldac332@gmail.com>
2026-02-12 23:07:48 +0100

    Architecture update: Migrate 'Editor' Model to Monolith Architecture, new service that call new repository

---
+++ server/src/modules/editor/editor.service.ts
@@ -0,0 +1,35 @@
+import {EditorRepository} from "./editor.repository.js";
+import {Editor} from "../../generated/prisma/client.js";
+
+import {CreateEditorDTO, UpdateEditorDTO} from "./editor.dto.js";
+
+export class EditorService {
+    private repo = new EditorRepository();
+
+    async getAllEditors(): Promise<Editor[]> {
+        return this.repo.findAll();
+    }
+
+    async getEditorById(id: string): Promise<Editor | null> {
+        return this.repo.findById(id);
+    }
+
+    async createEditor(dto: CreateEditorDTO): Promise<Editor> {
+        return this.repo.create({
+            name: dto.name
+        });
+    }
+
+    async updateEditor(id: string, dto: UpdateEditorDTO): Promise<Editor> {
+        return this.repo.update(id,dto);
+    }
+
+    async deleteEditor(id: string): Promise<Editor> {
+        return this.repo.delete(id);
+    }
+
+    async searchSimilarEditors(name: string): Promise<Editor[]> {
+        if (name.length < 2) return [];
+        return this.repo.findBySimilarityName(name);
+    }
+}
