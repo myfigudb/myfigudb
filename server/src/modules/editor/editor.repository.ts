9544f6a2470351ed4c80bb32e3b1f3a828782584
SolareFlame <zeldac332@gmail.com>
2026-02-12 23:07:33 +0100

    Architecture update: Migrate 'Editor' Model to Monolith Architecture, refractor service to split roles between repository and service.

---
+++ server/src/modules/editor/editor.repository.ts
@@ -0,0 +1,68 @@
+import {pclient} from "../../config/prisma.js";
+import {Editor, Prisma} from "../../generated/prisma/client.js";
+
+export class EditorRepository {
+
+    /**
+     * Retrieve an Editor by its unique ID.
+     */
+    async findById(id: string): Promise<Editor | null> {
+        return pclient.editor.findUnique({
+            where: { id }
+        });
+    }
+
+    /**
+     * Retrieve an Editor by its unique ID.
+     */
+    async findByName(name: string): Promise<Editor | null> {
+        return pclient.editor.findFirst({
+            where: { name }
+        });
+    }
+
+    /**
+     * Retrieve all Editors.
+     */
+    async findAll(): Promise<Editor[]> {
+        return pclient.editor.findMany();
+    }
+
+    /**
+     * Create a new Editor.
+     */
+    async create(data: Prisma.EditorCreateInput): Promise<Editor> {
+        return pclient.editor.create({
+            data: data
+        });
+    }
+
+    /**
+     * Update an existing Editor.
+     */
+    async update(id: string, data: Prisma.EditorUpdateInput): Promise<Editor> {
+        return pclient.editor.update({
+            where: { id },
+            data: data
+        });
+    }
+
+    /**
+     * Delete an Editor.
+     */
+    async delete(id: string): Promise<Editor> {
+        return pclient.editor.delete({
+            where: { id }
+        });
+    }
+
+    async findBySimilarityName(name: string, threshold: number = 0.3, limit: number = 1): Promise<Editor[]> {
+        return pclient.$queryRaw<Editor[]>`
+            SELECT *
+            FROM "editor"
+            WHERE similarity(name, ${name}) > ${threshold}
+            ORDER BY similarity(name, ${name}) DESC
+            LIMIT ${limit};
+        `;
+    }
+}
