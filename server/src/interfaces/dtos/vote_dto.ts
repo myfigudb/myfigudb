710ac7ef1583c95ac5f325e7f0d653cbe078e462
SolareFlame <zeldac332@gmail.com>
2026-02-13 02:37:32 +0100

    Architecture update: Delete useless files or double

--- server/src/interfaces/dtos/vote_dto.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const VoteSchema = z.object({
-    token: z.jwt(),
-    vote: z.boolean(), // true: Tag valid, false: Tag refused
-});
-export type VoteDTO = z.infer<typeof VoteSchema>;
