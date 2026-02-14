e9c8a612c48719c6c2d2a18cf01938a7bb24f197
SolareFlame <zeldac332@gmail.com>
2026-02-14 12:32:09 +0100

    Architecture update: Migrate 'Auth' Model to Monolith Architecture, refractor var to camalCase, updated dependencies

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import {Request, RequestHandler, Response} from 'express';
-import {AuthService} from "../services/database/user/authService.js";
-import {AuthDTO} from "../interfaces/dtos/auth_dto.js";
+import {AuthService} from "./auth.service.js";
+import {AuthDTO} from "./auth.dto.js";
 
 const service = new AuthService();
 
@@ -8,19 +8,19 @@ export class AuthController {
 
     login: RequestHandler<{}, any, AuthDTO> = async (req, res) => {
         try {
-            let access_token: string | null = null;
+            let accessToken: string | null = null;
 
             if (req.body.email) {
-                access_token = await service.loginWithEmail(req.body.email, req.body.password);
+                accessToken = await service.loginWithEmail(req.body.email, req.body.password);
             } else if (req.body.slug) {
-                access_token = await service.loginWithSlug(req.body.slug, req.body.password);
+                accessToken = await service.loginWithSlug(req.body.slug, req.body.password);
             }
 
-            if (!access_token) {
+            if (!accessToken) {
                 return res.status(401).json({ message: "Invalid credentials" });
             }
 
-            return res.status(200).json({ access_token });
+            return res.status(200).json({ accessToken });
 
         } catch (error) {
             console.error("Error logging in:", error);
