7bea85d510ad34371b7c2d4f0c51a836a58f79ec
Ewen <wenwengilbert@gmail.com>
2026-03-19 18:15:17 +0100

    edit app routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,10 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import { RequireAuth } from './components/auth/RequireAuth'
+import FiguresPage from './pages/FiguresPage'
+import FigureDetailsPage from './pages/FigureDetailsPage'
+import NotFound from './pages/NotFound'
 
 function App() {
     return (
@@ -9,6 +13,23 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route
+                path="/figures"
+                element={
+                    <RequireAuth>
+                        <FiguresPage />
+                    </RequireAuth>
+                }
+            />
+            <Route
+                path="/figures/:figureId"
+                element={
+                    <RequireAuth>
+                        <FigureDetailsPage />
+                    </RequireAuth>
+                }
+            />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     )
 }
